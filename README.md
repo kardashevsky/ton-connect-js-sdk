029ce39ffb0ac0b77c8e6e59bb3791f19ea0fa6b
d.kardashevskiy <d.kardashevskiy@neuragames.online>
2025-02-14 10:54:46 +0300

    fix

---
+++ README.md
@@ -0,0 +1,2 @@
+https://ton-connect.github.io/sdk/modules/_tonconnect_ui.html
+https://core.telegram.org/bots/webapps#initializing-mini-apps
--- script.js
+++ script.js
@@ -3,34 +3,27 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const tonConnect = new TON_CONNECT_UI.TonConnectUI({
         manifestUrl: "./tonconnect-manifest.json",
-        buttonRootId: "connectWallet" 
     });
 
-    const walletAddressEl = document.getElementById("walletAddress");
-    const openModalButton = document.getElementById("openModal");
+    const connectButton = document.getElementById("connectWallet");
 
-    console.log("TonConnectUI инициализирован");
-
-    const savedWallet = localStorage.getItem("ton_wallet");
-    if (savedWallet) {
-        console.log("Найден сохраненный кошелек: ", savedWallet);
-        walletAddressEl.textContent = "Подключенный кошелек: " + savedWallet;
-    }
+    connectButton.addEventListener("click", async () => {
+        try {
+            if (window.Telegram && Telegram.WebApp) {
+                Telegram.WebApp.HapticFeedback.impactOccurred("light");
+            }
+            console.log("Открытие модального окна TonConnect");
+            await tonConnect.openModal();
+        } catch (error) {
+            console.error("Ошибка при открытии модального окна:", error);
+        }
+    });
 
     tonConnect.onStatusChange((wallet) => {
         if (wallet) {
             console.log("Подключенный кошелек:", wallet);
             localStorage.setItem("ton_wallet", wallet.account.address);
             walletAddressEl.textContent = "Подключенный кошелек: " + wallet.account.address;
-        }
-    });
-
-    openModalButton.addEventListener("click", async () => {
-        try {
-            console.log("Открытие модального окна TonConnect");
-            await tonConnect.openModal();
-        } catch (error) {
-            console.error("Ошибка при открытии модального окна:", error);
         }
     });
 });
